9112b6ba565c39d6327edaac1627e7e93a1e9a3d
Shubham <sksingh94166@gmail.com>
2024-09-30 00:53:54 +0530

    Create consts.go

---
+++ consts.go
@@ -0,0 +1,37 @@
+// consts/consts.go
+
+package consts
+
+import (
+	"github.com/ava-labs/avalanchego/ids"
+	"github.com/ava-labs/avalanchego/vms/platformvm/warp"
+	"github.com/ava-labs/hypersdk/chain"
+	"github.com/ava-labs/hypersdk/codec"
+	"github.com/ava-labs/hypersdk/consts"
+)
+
+// information about our VM
+const (
+	HRP    = "token"   //TOKEN
+	Name   = "TokenVM" //SHUBHAM
+	Symbol = "TKNVM"   //SK
+)
+
+var ID ids.ID
+
+func init() {
+	b := make([]byte, consts.IDLen)
+	copy(b, []byte(Name))
+	vmID, err := ids.ToID(b)
+	if err != nil {
+		panic(err)
+	}
+	ID = vmID
+}
+
+// Instantiate registry here so it can be imported by any package. We set these
+// values in [controller/registry].
+var (
+	ActionRegistry *codec.TypeParser[chain.Action, *warp.Message, bool]
+	AuthRegistry   *codec.TypeParser[chain.Auth, *warp.Message, bool]
+)
